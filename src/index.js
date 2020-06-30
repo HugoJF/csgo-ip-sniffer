49936f08409acb57a8e8801ea8aa6b3d6c43f72f
Hugo Jeller <hugo_jeller@hotmail.com>
2020-06-30 17:36:09 -0400

    Track terminal width to avoid line breaks

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Gamedig from 'gamedig';
 import chalk from 'chalk';
 import sparkly from "sparkly";
 import Table from 'cli-table';
+import termSize from "term-size";
 
 const historySize = 10;
 
@@ -81,7 +82,16 @@ function increase(value) {
 }
 
 function render() {
-    let table = new Table({chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''}});
+    const {rows, columns} = termSize();
+    const checkColumns = 3;
+    const playersColumns = 13;
+    const sparkColumns = 13;
+    const nameColumns = columns - checkColumns - playersColumns - sparkColumns - 6;
+
+    const table = new Table({
+        chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''},
+        colWidths: [checkColumns, nameColumns, playersColumns, sparkColumns]
+    });
 
     Object.keys(servers).forEach((addr) => {
         let server = servers[addr];
@@ -93,6 +103,10 @@ function render() {
         let name = server.name || '?';
         let players = `${server.players || '0'} players`;
         let history = sparkly(server.history, {minimum: 0, maximum: server.maxPlayers});
+
+        if (name.length > nameColumns - 2) {
+            name = name.slice(0, nameColumns - 3) + '…';
+        }
 
         let info = [
             chalk.green(status),
