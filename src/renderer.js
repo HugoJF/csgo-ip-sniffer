0c2460348a865c27c2e603ad702d08da2aeaea2b
Hugo Jeller <hugo_jeller@hotmail.com>
2020-08-30 23:10:58 -0400

    Cleaned code

--- src/renderer.js
+++ src/renderer.js
@@ -5,17 +5,33 @@ import sparkly from "sparkly";
 import truncate from "cli-truncate";
 import {humanTime, increment, rotate} from "./helpers";
 
+const QUERY = 'query';
+const ONLINE = 'online';
+const OFFLINE = 'offline';
+
 const frames = ['-', '\\', '|', '/'];
 const tick = '√';
 const cross = '×';
 const noName = '?';
 
+const highlights = {
+    [QUERY]: chalk.blue,
+    [ONLINE]: chalk.white,
+    [OFFLINE]: chalk.red,
+};
+
+const playerWord = [
+    'player',
+    'players',
+];
+
+const checkColumns = 3;
+const playersColumns = 13;
+const sparkColumns = 13;
+
 export function renderTable(servers) {
     const {columns} = termSize();
-    const checkColumns = 3;
-    const playersColumns = 13;
-    const sparkColumns = 13;
-    const nameColumns = columns - checkColumns - playersColumns - sparkColumns - 6;
+    let nameColumns = columns - (checkColumns + 2) - (playersColumns + 2) - (sparkColumns + 2);
 
     const table = new Table({
         chars: {
@@ -32,13 +48,22 @@ export function renderTable(servers) {
 
     for (let server of servers) {
         let {querying, failed, frame, name, players, history, maxPlayers} = server;
-        let highlight = (text) => querying ? chalk.blue(text) : (failed ? chalk.red(text) : text);
-        let status = querying ? frames[frame] : failed ? cross : tick;
+        let state = querying ? QUERY : (failed ? OFFLINE : ONLINE);
+
+        let statusChars = {
+            [QUERY]: frames[frame],
+            [ONLINE]: tick,
+            [OFFLINE]: cross,
+        };
+
         let spark = sparkly(history, {
             minimum: 0,
             maximum: Math.max(...history, maxPlayers)
         });
 
+        let highlight = highlights[state];
+        let status = statusChars[state];
+        let word = playerWord[+(players !== 1)];
         let delta = 0;
         let duration = null;
         if (server.lastQueried) {
@@ -46,18 +71,19 @@ export function renderTable(servers) {
             duration = humanTime(delta);
         }
 
+        players = players || 0;
         server.frame = rotate(increment(frame), 0, frames.length - 1);
-        players = `${players || 0} players`;
+        players = [players, word].join(' ');
         name = truncate(name || noName, nameColumns - 2);
 
         let info = [
             chalk.green(status),
             name,
             players,
-            failed && duration ? Math.round(duration.duration) + duration.unit + ' offline' : spark,
+            failed && duration ? (Math.round(duration.duration) + duration.unit + ' offline') : spark,
         ];
 
-        table.push(info.map(highlight));
+        table.push(info.map(i => highlight(i)));
     }
 
     let total = Object.values(servers).reduce((acc, {players}) => acc + (players || 0), 0);
